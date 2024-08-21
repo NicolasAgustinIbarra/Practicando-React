cd9f5d5e7466ea1e4e380b76b99d3c84b2fe928c
nicolas ibarra <nicoibara099@gmail.com>
2024-08-21 13:41:11 -0300

    primera subida

---
+++ react1/src/App.jsx
@@ -0,0 +1,17 @@
+export function App(){
+    return(
+       <article>
+        <header>
+            <img src="https://media.licdn.com/dms/image/v2/D4D03AQE8t2inLe99pw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1709656370685?e=1729728000&v=beta&t=w1k1gXeMoZYreg2jyLjKrq_WdLPcdh4C1N4-uyqsj_k" 
+            alt="nico" />
+            <div>
+                <strong>Nico</strong>
+                <span>@nicoxd</span>
+            </div>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </header>
+       </article>
+    )
+}
